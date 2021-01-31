73cd9c113acdaca925b33a51536035472ee8009c
Vishal Shaha <vshahaqa@gmail.com>
2021-01-31 04:13:34 -0600

    initial

---
+++ FrameWorkDesign/src/test/java/Rough/Batch1.java
@@ -0,0 +1,33 @@
+package Rough;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Batch1 {
+
+	public WebDriver driver;
+
+	@Test
+	public void test1() {
+
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+
+		driver.get("https://www.facebook.com/");
+
+	}
+
+	@Test
+	public void test2() {
+
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+
+		driver.get("https://www.gmail.com/");
+
+	}
+
+}
